c4dda12c09f29ba4fd5362c5a4e186795105e876
Anderson Gonçalves <77304321+AndersonGACFilho@users.noreply.github.com>
2023-11-21 20:01:52 -0300

    new topics, subs and pubs

--- publisher2.py
+++ publisher2.py
@@ -6,6 +6,6 @@ s = context.socket(zmq.PUB)        # create a publisher socket
 p = "tcp://0.0.0.0:"+ PORT      # how and where to communicate
 s.bind(p)                          # bind socket to the address
 while True:
-	time.sleep(5)                    # wait every 5 seconds
-	msg = str.encode("Publisher 1, time each 5 seconds: " + time.asctime())
+	time.sleep(8)                    # wait every 8 seconds
+	msg = str.encode("Publisher 2, time each 8 seconds" + time.asctime())
 	s.send(msg) # publish the current time
--- subscriber3.py
+++ subscriber3.py
@@ -7,6 +7,11 @@ p1 = "tcp://"+ HOST1 +":"+ PORT        # how and where to communicate
 s1.connect(p1)                         # connect to the server
 s1.setsockopt_string(zmq.SUBSCRIBE, "TIME")  # subscribe to TIME messages
 
+s2 = context.socket(zmq.SUB)          # create a subscriber socket
+p2 = "tcp://"+ HOST2 +":"+ PORT        # how and where to communicate
+s2.connect(p2)                         # connect to the server
+s2.setsockopt_string(zmq.SUBSCRIBE, "TIME")  # subscribe to TIME messages
+
 for i in range(5):  # Five iterations
-	time = s.recv()   # receive a message
+	time = s1.recv() + "\r\n" + s2.recv()   # receive a message
 	print (bytes.decode(time))
